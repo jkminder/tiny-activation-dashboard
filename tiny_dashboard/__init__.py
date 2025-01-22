7290b20cc910558c47fc64808ab0565089b69937
Clement Dumas <butanium.contact@gmail.com>
2025-01-21 20:56:22 -0800

    added activation visualization

--- tiny_dashboard/__init__.py
+++ tiny_dashboard/__init__.py
@@ -3,9 +3,11 @@ from .feature_centric_dashboards import (
     OnlineFeatureCentricDashboard,
     AbstractOnlineFeatureCentricDashboard,
 )
+from .visualization_utils import activation_visualization
 
 __all__ = [
     "OfflineFeatureCentricDashboard",
     "OnlineFeatureCentricDashboard",
     "AbstractOnlineFeatureCentricDashboard",
+    "activation_visualization",
 ]
